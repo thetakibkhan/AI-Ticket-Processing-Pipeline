0bdadbc8e7b7c7dfad4743cee871863c917b970e
thetakibkhan <takib.khan.01@gmail.com>
2026-04-29 23:14:40 +0600

    feat: add helmet middleware for enhanced security

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 import ticketRoutes from './routes/tickets.js';
+import helmet from 'helmet';
 
 const app = express();
+
+app.use(helmet());
+
 app.use(express.json());
 
 app.use('/tickets', ticketRoutes);
